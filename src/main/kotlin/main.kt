bd78155a2cde8d9acf41f09517d081ed6ba3193d
iamarjun <arjunmanoj1995@gmail.com>
2020-11-27 11:56:34 +0530

    nested functions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,15 @@
 fun main(args: Array<String>) {
     println("Hello World!")
+
+    fun hello() {
+        println("Hello")
+
+        fun moo() {
+            println("Moo")
+        }
+
+        moo()
+    }
+
+    hello()
 }
