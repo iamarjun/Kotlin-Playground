fa91c3dc26f58a2b7d08e1dc67577833559b6e2c
iamarjun <arjunmanoj1995@gmail.com>
2020-11-30 12:27:29 +0530

    nested classes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,28 @@
 fun main() {
-    val friend = User(firstName = "Shivangi", lastName = "Shah")
+
+
+    /**
+     * Nested class don't hold a reference to the parent class
+     */
+    val myCar = Vehicle()
+    val steeringWheel = Vehicle.SteeringWheel()
+    val transmission = Vehicle.Transmission()
+
+}
+
+class Vehicle {
+
+    var brand: String = "unknown"
+    fun info() = println(brand)
+
+    class SteeringWheel {
+        var name: String = "Steering Wheel"
+        fun info() = println(name)
+    }
+
+    class Transmission {
+        var type = "Automatic"
+        fun info() = println("The vehicle has shifted")
+    }
 }
 
