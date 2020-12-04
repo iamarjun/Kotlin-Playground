ff9b2b042ef59fcd490d73351b3483a4040db47a
iamarjun <arjunmanoj1995@gmail.com>
2020-12-04 16:22:28 +0530

    destructuring data classes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,13 @@
 fun main() {
     val a = User("arjun", 10)
-
-    val (name, age) = a
     val b = a.copy()
     println(a == b)
-    println(name)
-    println(age)
+
+    val c = Pair("Arjun", 10)
+    val d = Triple("Arjun", false, 25)
+
+    val (title, years) = c
+    val (name, isSingle, age) = d
 }
 
 data class User( val name: String,  val age: Int)
